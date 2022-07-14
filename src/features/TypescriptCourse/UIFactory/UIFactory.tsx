6ee02dd218af27d6bbbf0d8644c11b4d6b479d41
Androsov-Stepan <stapan.news@yandex.ru>
2022-07-14 08:57:44 +0300

    added radio btns

--- src/features/TypescriptCourse/UIFactory/UIFactory.tsx
+++ src/features/TypescriptCourse/UIFactory/UIFactory.tsx
@@ -1,4 +1,6 @@
-import { Divider, Collapse } from 'antd'
+import { Divider, Collapse, Checkbox, Button } from 'antd'
+import { MyArrowIcon } from '../../../components/ui/ArrowIcon/MyArrowIcon'
+import { ChooseComponent } from './ChooseComponent/ChooseComponent'
 import style from './UIFactory.module.css'
 
 const { Panel } = Collapse
@@ -9,7 +11,20 @@ export const UIFactory = () => {
       <Panel header="UIFactory" key="2">
         <h2>UI Factory</h2>
         <p>In this task I need to create UI Factory</p>
-        <div className={style.content}></div>
+        <div className={style.uiBlock}>
+          <div className={style.content}>
+            <ChooseComponent />
+            <Button className={style.btn} type="primary">
+              Create
+            </Button>
+          </div>
+          <div className={style.component}>
+            <MyArrowIcon direction="up" />
+            <MyArrowIcon direction="right" />
+            <MyArrowIcon direction="down" />
+            <MyArrowIcon direction="left" />
+          </div>
+        </div>
 
         <Divider />
       </Panel>
