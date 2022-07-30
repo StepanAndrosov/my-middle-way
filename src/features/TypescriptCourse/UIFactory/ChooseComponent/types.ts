2b5527ffd5689b413d49632ae71217e011a5db06
Androsov-Stepan <stapan.news@yandex.ru>
2022-07-30 12:18:39 +0300

    removed types

--- src/features/TypescriptCourse/UIFactory/ChooseComponent/types.ts
+++ src/features/TypescriptCourse/UIFactory/ChooseComponent/types.ts
@@ -1,36 +1,7 @@
 
-import {MyArrowIcon} from '../../../../components/ui/ArrowIcon/MyArrowIcon'
-import {Alert, Button} from 'antd'
-
-
-
-type ButtonType = typeof Button
-type AlertType = typeof Alert
-type Arrow = typeof MyArrowIcon
-
-type MyComponents = ButtonType | AlertType | Arrow
 
 type CompNames = 'Button' | 'Alert' | 'Arrow'
 
-type BtnProps = 'primary' | 'default'
-type AlertProps = 'success' | 'info' | 'warning' | 'error'
-type ArrowProps = 'up' | 'right' | 'left' | 'down'
-
-type UnionProps = BtnProps | AlertProps | ArrowProps
-
-
-interface UiFactory {
-    button: ButtonType;
-    alert: AlertType;
-    arrow: Arrow;
-  }
- 
-
-
 export type {
-    UiFactory,
-    MyComponents,
-    CompNames,
-    UnionProps,
-    BtnProps,  AlertProps,  ArrowProps
+    CompNames
 }
