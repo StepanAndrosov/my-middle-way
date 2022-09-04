8e3dff948db2a68eb49e1a41d3709574556c2a22
Androsov-Stepan <stapan.news@yandex.ru>
2022-09-04 09:43:58 +0300

    added collapse auction

---
+++ src/features/Solidity/DuchAuction/DuchAuction.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Divider, Collapse } from 'antd'
+
+const { Panel } = Collapse
+
+export const DuchAuction = React.memo(() => {
+  return (
+    <Collapse>
+      <Panel header="DuchAuction" key="1">
+        <h2>Duch Auction</h2>
+      </Panel>
+
+      <Divider />
+    </Collapse>
+  )
+})
--- src/features/Solidity/Solidity.tsx
+++ src/features/Solidity/Solidity.tsx
@@ -1,9 +1,11 @@
 import style from './Solidity.module.css'
+import { DuchAuction } from './DuchAuction/DuchAuction'
 
 export const Solidity: React.FC = () => {
   return (
     <div className={style.Course}>
       <h1>Solidity</h1>
+      <DuchAuction />
     </div>
   )
 }
