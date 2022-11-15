2c5b3502aa798af72968828dc076a4afdac00c2b
Androsov-Stepan <stapan.news@yandex.ru>
2022-11-15 09:47:17 +0300

    listener factory done

--- src/features/TSCourse/ListenerFactory/ChooseElement.tsx
+++ src/features/TSCourse/ListenerFactory/ChooseElement.tsx
@@ -1,65 +1,44 @@
-import { Button, Select, Input, Checkbox } from 'antd'
-import type { CheckboxChangeEvent } from 'antd/es/checkbox'
+import type { RadioChangeEvent } from 'antd'
+import { Button, Radio, Select } from 'antd'
 import { useState } from 'react'
 import { successMessage } from '../../../utils/messsage-utils'
 import style from './ListenerFactory.module.css'
 
-const { TextArea } = Input
-
-export interface ListenerFactory {
-  makeButtonClick: () => void
-  makeInputInput: () => void
-  makeSelectInput: () => void
-  makeTextAreaInput: () => void
-}
-export interface Subscribe {
-  (
-    target: typeof Button,
-    name: 'onClick',
-    listener: ReturnType<ListenerFactory['makeButtonClick']>
-  ): void
-  (
-    target: typeof Input,
-    name: 'onInput',
-    listener: ReturnType<ListenerFactory['makeInputInput']>
-  ): void
-  (
-    target: typeof Select,
-    name: 'onChange',
-    listener: ReturnType<ListenerFactory['makeSelectInput']>
-  ): void
-  (
-    target: typeof TextArea,
-    name: 'onInput',
-    listener: ReturnType<ListenerFactory['makeTextAreaInput']>
-  ): void
-}
+const { Option } = Select
 
 export const ChooseElement = () => {
   const [elem, setElem] = useState('')
 
-  const chooseElem = (
-    type: 'button' | 'select' | 'input' | 'textarea',
-    check: CheckboxChangeEvent
-  ) => {
-    if (check.target.checked)
-      switch (type) {
-        case 'button':
-          setElem(type)
-          break
-      }
-    else setElem('')
+  const chooseElem = (e: RadioChangeEvent) => {
+    setElem(e.target.value)
   }
 
   return (
     <div>
-      <h2>create element</h2>
+      <h2>Create element</h2>
       <div className={style.elementBlock}>
-        <Checkbox onChange={(check) => chooseElem('button', check)}>
-          Choose Button
-        </Checkbox>
-        <div>
-          {elem === 'button' ? <Button>Click</Button> : 'choose element'}
+        <Radio.Group onChange={chooseElem}>
+          <Radio value={'button'}>Choose Button</Radio>
+          <Radio value={'select'}>Choose Select</Radio>
+        </Radio.Group>
+
+        <div className={style.Element}>
+          {elem === 'button' && (
+            <Button onClick={() => successMessage('click')}>Click</Button>
+          )}
+        </div>
+        <div className={style.Element}>
+          {elem === 'select' && (
+            <Select
+              onChange={(value) => successMessage(`selected ${value}`)}
+              style={{ width: 100 }}
+              defaultValue="1"
+            >
+              <Option value="1">option 1</Option>
+              <Option value="2">option 2</Option>
+              <Option value="3">option 3</Option>
+            </Select>
+          )}
         </div>
       </div>
     </div>
