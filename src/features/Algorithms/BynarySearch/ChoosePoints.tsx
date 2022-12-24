4f06164876bdaa870f9cea20d23b56a8f8b850e0
Androsov-Stepan <stapan.news@yandex.ru>
2022-12-24 08:31:29 +0300

    added choose points

--- src/features/Algorithms/BynarySearch/ChoosePoints.tsx
+++ src/features/Algorithms/BynarySearch/ChoosePoints.tsx
@@ -1,18 +1,41 @@
-import { memo } from 'react'
+import { memo, useState } from 'react'
 import { lastPlayerPoints, firstPlayerPoints } from './selectors'
 import { useSelector } from 'react-redux'
 import styles from './BynarySearch.module.css'
+import { InputNumber, Button } from 'antd'
 
 export const ChoosePoints = memo(() => {
   const firstPlace = useSelector(firstPlayerPoints)
   const lastPlace = useSelector(lastPlayerPoints)
+
+  const [points, setPoints] = useState(0)
+
+  const onChange = (value: number) => {
+    setPoints(value)
+  }
+
   return (
     <div className={styles.choose}>
       Choose points between:
       <div>
-        <span className={styles.chooseElememt}>{lastPlace} p.</span>
-        <span className={styles.chooseElememt}>{firstPlace} p.</span>
+        <span className={styles.chooseElememt}>
+          from {lastPlace} {lastPlace === 1 ? 'point' : 'points'}
+        </span>
+        <span className={styles.chooseElememt}>
+          to {firstPlace} points
+        </span>
       </div>
+      <div className={styles.Input}>
+        <InputNumber
+          max={firstPlace}
+          min={lastPlace}
+          onChange={onChange}
+        />
+      </div>
+      {points}
+      <Button type="primary" disabled={!points}>
+        choose
+      </Button>
     </div>
   )
 })
